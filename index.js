6d362d231954a82dededd4cd64c99b29a1f623b1
jihad-A-G <jihadabdlghani73@gmail.com>
2023-09-22 09:59:56 +0300

    step 3

--- index.js
+++ index.js
@@ -2,9 +2,25 @@ import express from "express";
 
 const app = express();
 
+app.use("/time", (req,res,next)=>{
+    let date=new Date();
+    let time=`${date.getHours()} : ${date.getMinutes()}`
+    res.send(`{status:${res.statusCode}, message: ${time}}`);
+})
+
+app.use("/test", (req,res,next)=>{
+    res.send(`{status:${res.statusCode}, message:"ok"}`);
+    console.log(res);
+
+})
+
 app.use("/",(req,res,next)=>{
     res.send("OK");
 })
+
+
+
+
 
 app.listen(3000,(err)=>{
     if(err){
