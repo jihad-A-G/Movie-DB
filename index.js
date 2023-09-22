7ae573efdd2dda54da244db15ed349c5bfd2b2b6
jihad-A-G <jihadabdlghani73@gmail.com>
2023-09-22 10:35:23 +0300

    step 4

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ import express from "express";
 
 const app = express();
 
+app.use("/hello/:id",(req,res,next) =>{
+    let id= req.params.id;
+    res.send(`{status:${res.statusCode}, message: Hello, ${id}}`)
+})
+app.use("/search",(req,res,next)=>{
+    let search=req.query.s;
+    if(search){
+        res.status(200).send(`{status:${res.statusCode}, message: "ok", data: ${search}}`);
+    }else{
+        res.status(500).send(`{status:${res.statusCode}, error: true, message: "you have to provide a search"}`);
+
+    }
+
+})
 app.use("/time", (req,res,next)=>{
     let date=new Date();
     let time=`${date.getHours()} : ${date.getMinutes()}`
