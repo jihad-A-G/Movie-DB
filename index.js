d856b490d17d25b409e1c4af5e937fe624c48175
jihad-A-G <jihadabdlghani73@gmail.com>
2023-09-22 10:48:02 +0300

    step 5

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
 
 const app = express();
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
+]
+
+app.use("/movies/get",(req,res,next) =>{
+    res.status(200).send(`{status:${res.statusCode}, data: ${movies.map(e=>{return ` title: ${e.title}, year: ${e.year}, rating: ${e.rating}||`})}}`)
+})
 
 app.use("/hello/:id",(req,res,next) =>{
     let id= req.params.id;
-    res.send(`{status:${res.statusCode}, message: Hello, ${id}}`)
+    if(id){
+        res.send(`{status:${res.statusCode}, message: Hello, ${id}}`)
+    }else{
+        res.send(`{status:${res.statusCode}, message: Hello}}`)
+
+    }
 })
 app.use("/search",(req,res,next)=>{
     let search=req.query.s;
